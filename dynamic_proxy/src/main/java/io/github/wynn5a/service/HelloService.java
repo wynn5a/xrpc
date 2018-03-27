8dbf4a145ed3a95717c08603439856911b71e4ea
wynn-5a <winminy@163.com>
2018-03-27 16:56:29 +0800

    using dynamic proxy

---
+++ dynamic_proxy/src/main/java/io/github/wynn5a/service/HelloService.java
@@ -0,0 +1,15 @@
+package io.github.wynn5a.service;
+
+/**
+ * @author Fuwenming
+ * @create 2018/3/27
+ **/
+public interface HelloService {
+    /**
+     * say hello to the name
+     *
+     * @param name
+     * @return
+     */
+    String sayHello(String name);
+}
